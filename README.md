fff00d71efdd540e90424ae7efa211aec1c370f1
endail <52652357+endail@users.noreply.github.com>
2021-05-24 13:51:00 +1000

    .

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# hx711-arduino-i2c-master
- 
+# HX711 Arduino I2C Master
+
+This is an example implementation of how an I2C master might communicate with [the I2C slave](https://github.com/endail/hx711-arduino-i2c-slave) to retrieve HX711 data.
+
+I will update this repository with more usable code in the future.
+
+## Sample Output from Master Sketch
+
+```shell
+Diff: 6ms ago, Reading: 16410197
+Diff: 4ms ago, Reading: 16409850
+Diff: 13ms ago, Reading: 16410244
+Diff: 8ms ago, Reading: 16410404
+Diff: 4ms ago, Reading: 16410603
+Diff: 2ms ago, Reading: 16410689
+Diff: 8ms ago, Reading: 16408988
+Diff: 4ms ago, Reading: 16409260
+Diff: 2ms ago, Reading: 16409204
+Diff: 10ms ago, Reading: 16410105
+```
---
+++ master/master.ino
@@ -0,0 +1,67 @@
+// MIT License
+//
+// Copyright (c) 2021 Daniel Robertson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <Wire.h>
+
+#define BAUD_RATE 115200
+#define I2C_SLAVE_ADDR 0x6e
+#define I2C_SLAVE_BYTES 5
+#define DELAY_MS 200
+
+void setup() {
+    while(!Serial);
+    Serial.begin(BAUD_RATE);
+    Wire.begin();
+}
+
+void loop() {
+
+    Wire.requestFrom(I2C_SLAVE_ADDR, I2C_SLAVE_BYTES);
+
+    uint8_t arr[I2C_SLAVE_BYTES]{0};
+
+    for(uint8_t* ptr = arr; ptr < arr + I2C_SLAVE_BYTES; ++ptr) {
+        *ptr = (uint8_t)Wire.read();
+    }
+
+    //reconstruct the data
+
+    //time diff
+    const uint16_t timeDiff = 
+        (static_cast<uint16_t>(arr[0]) << 8) |
+         static_cast<uint16_t>(arr[1]);
+
+    //hx711 reading
+    int32_t rd = 
+        (static_cast<int32_t>(     0) << 24) |
+        (static_cast<int32_t>(arr[2]) << 16) |
+        (static_cast<int32_t>(arr[3]) << 8)  |
+        (static_cast<int32_t>(arr[4]) << 0);
+
+    Serial.print("Diff: ");
+    Serial.print(timeDiff, DEC);
+    Serial.print("ms ago, Reading: ");
+    Serial.println(rd, DEC);
+
+    delay(DELAY_MS);
+
+}
